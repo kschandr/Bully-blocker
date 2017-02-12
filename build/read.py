430df81a498d055ada1e7d443870c37a5de070d6
kschandr <mykaysha@gmail.com>
2017-02-12 07:30:25 -0800

    initial commit

---
+++ build/read.py
@@ -0,0 +1,8 @@
+import json
+import pandas as pd
+
+strang = "this is bad so horrible";
+punctuation = '`~!@#$%^&*()_-+={[}]|\:;"<,>.?/}\t\n'
+data = json.load(affin.json)
+if x in strang.split().strip(punctuation):
+
